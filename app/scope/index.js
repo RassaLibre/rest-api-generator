86f5e89cab5b94312db3e002682582d5b64ad58c
Tomas Prochazka <tomas@searis.no>
2015-01-24 18:29:45 +0100

    scope + tests

--- app/scope/index.js
+++ app/scope/index.js
@@ -1,4 +1,50 @@
+var _ = require('underscore');
 
-var scope = function(){};
+
+/**
+* The prototype represents scope on which all the templates will be executed
+* @constructor
+* @param {Object} model created in the editor
+*/
+var scope = function(model){
+  
+  /**
+  * model created in the editor
+  * @type {Object}
+  */
+  this.model = model;
+
+  /**
+  * object stores all the additional variables that will be attached to
+  * the scope
+  * @type {Object}
+  */
+  this.scope_variables_ = {};
+};
+
+/**
+* function registers new scope variables with a passed key. This key has
+* to be unique.
+* @param {String} key
+* @param {String|Function|Object} variable
+* @return {Bool} scope variable registered? 
+*/
+scope.prototype.register_scope_variable = function(key, variable){
+  if((!_.isUndefined(this.scope_variables_[key]))||(key === "model")) return false;
+  else{
+    this.scope_variables_[key] = variable;
+    return true;
+  }
+};
+
+/**
+* function returns complete scope ready to be used in the templates
+* @return {Object}
+*/
+scope.prototype.get_scope = function(){
+  var scope = {};
+  scope.model = this.model;
+  return _.extend(scope, this.scope_variables_);
+};
 
 module.exports = scope;
---
+++ test/scope.js
@@ -0,0 +1,53 @@
+var assert = require("assert"); // node.js core module
+var Scope = require('../app/scope');
+var example_data = require('./example_data');
+var _ = require('underscore');
+
+describe('Scope', function(){
+
+  var scope;
+
+  beforeEach(function(){
+    scope = new Scope(example_data);
+  });
+
+  it('should be defined', function(){
+    assert.equal(typeof scope, "object");
+  });
+
+  it('should register new scope variable', function(){
+    var res = scope.register_scope_variable('new_one','Tomas');
+    assert.equal(res, true);
+    assert.equal(scope.scope_variables_['new_one'], "Tomas");
+  });
+
+  it('should not allow to register property named "model" to scope', function(){
+    var res = scope.register_scope_variable('model','Tomas');
+    assert.equal(res, false);
+    assert.equal(scope.scope_variables_['model'], undefined);
+  });
+
+  it('should not allow to register already existing property', function(){
+    var res = scope.register_scope_variable('new_one','Tomas');
+    assert.equal(res, true);
+    res = scope.register_scope_variable('new_one','Tomas');
+    assert.equal(res, false);  
+  });
+
+  it('should return the complete scope', function(){
+    scope.register_scope_variable('first','Tomas');
+    scope.register_scope_variable('second','Tore');
+    scope.register_scope_variable('third','Tor-Inge');
+    scope.register_scope_variable('fourth','Bernt');
+    var returned_scope = scope.get_scope();
+    assert.equal(typeof returned_scope, "object");
+    assert.equal(_.keys(returned_scope).length, 5);
+    assert.equal(_.isUndefined(returned_scope.model), false);
+    assert.equal(returned_scope.first, "Tomas");
+    assert.equal(returned_scope.second, "Tore");
+    assert.equal(returned_scope.third, "Tor-Inge");
+    assert.equal(returned_scope.fourth, "Bernt");  
+  });
+
+
+});
