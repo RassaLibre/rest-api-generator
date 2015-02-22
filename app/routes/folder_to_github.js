d1763ebb434a79725784cc5e56271f485945105d
Tomas Prochazka <tomas@searis.no>
2015-02-22 23:13:03 +0100

    branching works

--- app/routes/folder_to_github.js
+++ app/routes/folder_to_github.js
@@ -21,94 +21,71 @@ var generate_handlers = {
     var model = req.body;
     var metamodel = new Metamodel();
     if(metamodel.validate(model)){
-      var scope = new Scope(model);
-      //load the templates
-      var template_loader = new Template_Loader(config.TEMPLATE_DIR, config.TEMPLATE_CONFIG_FILE_NAME);
-      template_loader.load_config_file();
-      template_loader.load_atomic_templates();
-      template_loader.load_normal_templates();
-      template_loader.load_duplicated_templates();
-      var atomic_templates = template_loader.get_atomic_templates();
-      //register atomic templates in scope
-      scope.register_scope_array(atomic_templates);
-      var duplicated_templates = template_loader.get_duplicated_templates();
-      var normal_templates = template_loader.get_normal_templates();
-      //execute the templates
-      var template_executor = new Template_Executor(duplicated_templates, normal_templates ,scope.get_scope());
-      template_executor.execute_duplicated_templates();
-      template_executor.execute_normal_templates();
-      normal_templates = template_executor.get_normal_templates();
-      duplicated_templates = template_executor.get_duplicated_templates();
-      //beutify the templates
-      var template_beautifier = new Template_Beautifier(normal_templates, duplicated_templates);
-      template_beautifier.beautify_normal_templates();
-      template_beautifier.beautify_duplicated_templates();
-      normal_templates = template_beautifier.get_normal_templates();
-      duplicated_templates = template_beautifier.get_duplicated_templates();
-      //save the templates
-      var template_saver = new Template_Saver(duplicated_templates, normal_templates, config.OUTPUT_DIR);
-      template_saver.save_duplicated_templates();
-      template_saver.save_normal_templates();
-
-
       var repo = new git('generated/api/');
-      repo.branch("master",function(err){
-
-        var branch_name = new Date();
-        branch_name = branch_name.toISOString().replace(":","-").substring(0,16);
-        repo.create_branch(branch_name,function(err){
-          repo.branch(branch_name, function(err){
-
-            var changes = false;  //flag indicates if there are any files with change, if so uload to github
-            var options = {}, args = ["--name-only"];
-            repo.git("diff", options, args, function(err, stdout, stderr) {
-              changes = true;
-              console.log(stdout);
-              repo.add(stdout,function(err){
-                console.log(err);
-              });
-            });
-
-            setTimeout(function(){
-              if(changes){
-                repo.commit("Generated: "+Date.now(),function(err){
-                  if(err) console.log(err);
-                  repo.remote_push("origin",branch_name,function(err){
-                    if(err) console.log(err);
-                    res.send('have no idea, look to the console');
-                  });
-                });
-              }
-              else{
-                res.send('no change, no github, remove the brunch');
-              }
-            },2000);
-
-          });        
-        });
-
-      });
-
-
-      
-          /**
       var branch_name = new Date();
       branch_name = branch_name.toISOString().replace(":","-").substring(0,16);
       repo.create_branch(branch_name,function(err){
-        repo.branch(branch_name, function(err){
-          repo.commit("Generated: "+Date.now(),function(err){
-            if(err) console.log(err);
-            repo.remote_push("origin","master",function(err){
-              if(err) console.log(err);
-              res.send('have no idea, look to the console');
+        if(err) console.log(err);
+        repo.checkout(branch_name, function(err){
+          if(err) console.log(err);
+          var scope = new Scope(model);
+          //load the templates
+          var template_loader = new Template_Loader(config.TEMPLATE_DIR, config.TEMPLATE_CONFIG_FILE_NAME);
+          template_loader.load_config_file();
+          template_loader.load_atomic_templates();
+          template_loader.load_normal_templates();
+          template_loader.load_duplicated_templates();
+          var atomic_templates = template_loader.get_atomic_templates();
+          //register atomic templates in scope
+          scope.register_scope_array(atomic_templates);
+          var duplicated_templates = template_loader.get_duplicated_templates();
+          var normal_templates = template_loader.get_normal_templates();
+          //execute the templates
+          var template_executor = new Template_Executor(duplicated_templates, normal_templates ,scope.get_scope());
+          template_executor.execute_duplicated_templates();
+          template_executor.execute_normal_templates();
+          normal_templates = template_executor.get_normal_templates();
+          duplicated_templates = template_executor.get_duplicated_templates();
+          //beutify the templates
+          var template_beautifier = new Template_Beautifier(normal_templates, duplicated_templates);
+          template_beautifier.beautify_normal_templates();
+          template_beautifier.beautify_duplicated_templates();
+          normal_templates = template_beautifier.get_normal_templates();
+          duplicated_templates = template_beautifier.get_duplicated_templates();
+          //save the templates
+          var template_saver = new Template_Saver(duplicated_templates, normal_templates, config.OUTPUT_DIR);
+          template_saver.save_duplicated_templates();
+          template_saver.save_normal_templates();
+          //find the changed files
+          var changes = false;  //flag indicates if there are any files with change, if so uload to github
+          var options = {}, args = ["--name-only"];
+          repo.git("diff", options, args, function(err, stdout, stderr) {
+            changes = true;
+            console.log(stdout);
+            repo.add(stdout,function(err){
+              console.log(err);
             });
           });
-        });        
+          //push it
+          setTimeout(function(){
+            if(changes){
+              repo.commit("Generated: "+Date.now(),function(err){
+                if(err) console.log(err);
+                repo.remote_push("origin",branch_name,function(err){
+                  if(err) console.log(err);
+                  res.send('have no idea, look to the console');
+                });
+              });
+            }
+            else{
+              res.send('no change, no github, remove the brunch');
+            }
+          },2000);
+
+        });
       });
-**/
     }
   }
-
 };
 
 module.exports = generate_handlers;
--- templates/api/README.md
+++ templates/api/README.md
@@ -1,2 +1,2 @@
 #GENERATED REST API
-Generated API from the generator.
+Generated API from the generator. Aaaand this is just because I need some change in the templates!
